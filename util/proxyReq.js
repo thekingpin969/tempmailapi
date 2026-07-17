97dd34a56afe0716decadaf81302be318f89570d
thekingpin969 <thekingpin969@gmail.com>
2026-07-17 22:21:04 +0530

    proxy servervice changed

--- util/proxyReq.js
+++ util/proxyReq.js
@@ -1,22 +1,23 @@
 import axios from 'axios';
 import { getKey, releaseKey, useCredits } from './apiKeyManager.js';
 
-const SCRAPER_API_BASE = 'https://api.scraperapi.com/';
+const SCRAPER_API_BASE = 'http://api.scrape.do/';
 const DEFAULT_COST = 10;
 
 async function proxyReq(params, keep_headers = true) {
   let apiKey = null;
-  
+
   try {
     apiKey = await getKey();
-    
-    const proxyUrl = `${SCRAPER_API_BASE}?api_key=${apiKey}&url=${params.url}&keep_headers=${keep_headers}`;
+
+    const proxyUrl = `${SCRAPER_API_BASE}?url=${params.url}&token=${apiKey}&forwardHeaders=${keep_headers}&super=true`;
     const response = await axios({ ...params, url: proxyUrl });
-    
+
     await useCredits(apiKey, DEFAULT_COST);
-    
+
     return response;
   } catch (error) {
+    console.log(error)
     if (error.response) {
       throw error.response.data || error.response;
     }
